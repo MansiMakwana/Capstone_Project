1e7813842ea6e91e50c1722f228970fe045aff94
LTI-Intern-2021 <lntinterns2021@gmail.com>
2021-04-29 12:36:28 +0530

    Added Get and Post Mthods

---
+++ Spring-Application/src/main/java/com/example/SpringApplication/Contoller/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.SpringApplication.Contoller;
+
+import com.example.SpringApplication.Entity.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+@GetMapping
+    public List<Student> getAllStudents(){
+    return null;
+}
+
+@PostMapping
+    public Student postStudent(@RequestBody Student student){
+    return null;
+    }
+
+}
---
+++ Spring-Application/src/main/java/com/example/SpringApplication/Entity/Student.java
@@ -0,0 +1,32 @@
+package com.example.SpringApplication.Entity;
+
+public class Student {
+
+    private Integer studentId;
+    private String studentName;
+    private String studentDept;
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentDept() {
+        return studentDept;
+    }
+
+    public void setStudentDept(String studentDept) {
+        this.studentDept = studentDept;
+    }
+}
